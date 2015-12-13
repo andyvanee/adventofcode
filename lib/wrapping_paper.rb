34c5c7b54c9b84f4724cc61b679de3e324bffe5f
Andy VanEe <1andyvanee@gmail.com>
2015-12-12 21:53:12 -0700

    Day 2: Step 2

--- lib/wrapping_paper.rb
+++ lib/wrapping_paper.rb
@@ -1,37 +1,56 @@
 module WrappingPaper
-  # Every present is a box (a perfect right rectangular prism), which makes
-  # calculating the required wrapping paper for each gift a little easier: find
-  # the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l.
-  def self.prism_area(shape)
-    l,w,h = shape
-    2*l*w + 2*w*h + 2*h*l
-  end
-
-  # The elves also need a little extra paper for each present: the area of the
-  # smallest side.
-  def self.paper_extra(shape)
-    l, w = smallest_sides(shape)
-    l * w
-  end
-
-  # The actual area plus the extra
-  def self.paper_area(shape)
-    prism_area(shape) + paper_extra(shape)
-  end
-
-  # The sum of all given shapes
-  def self.sum(shapes)
-    total = 0
-    shapes.each do |line|
-      l, w, h = line.split('x').map { |e| e.to_i }
-      total += paper_area([l,w,h])
+    # Every present is a box (a perfect right rectangular prism), which makes
+    # calculating the required wrapping paper for each gift a little easier: find
+    # the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l.
+    def self.prism_area(shape)
+        l,w,h = shape
+        2*l*w + 2*w*h + 2*h*l
     end
-    total
-  end
 
-  def self.smallest_sides(shape)
-    shape.sort[0..1]
-  end
-  private_class_method :smallest_sides
+    # The elves also need a little extra paper for each present: the area of the
+    # smallest side.
+    def self.paper_extra(shape)
+        l, w = smallest_sides(shape)
+        l * w
+    end
+
+    # The actual area plus the extra
+    def self.paper_area(shape)
+        prism_area(shape) + paper_extra(shape)
+    end
+
+    # The sum of all given shapes
+    def self.sum(shapes)
+        total = 0
+        shapes.each do |line|
+            l, w, h = line.split('x').map { |e| e.to_i }
+            total += paper_area([l,w,h])
+        end
+        total
+    end
+
+    def self.ribbon_sum(shapes)
+        total = 0
+        shapes.each do |line|
+            shape = line.split('x').map { |e| e.to_i }
+            total += ribbon_length(shape) + bow_length(shape)
+        end
+        total
+    end
+
+    def self.ribbon_length(shape)
+        l,w = smallest_sides(shape)
+        return l*2 + w*2
+    end
+
+    def self.bow_length(shape)
+        l,w,h = shape
+        return l*w*h
+    end
+
+    def self.smallest_sides(shape)
+        shape.sort[0..1]
+    end
+    private_class_method :smallest_sides
 end
 
--- spec/wrapping_paper_spec.rb
+++ spec/wrapping_paper_spec.rb
@@ -2,31 +2,48 @@ require 'minitest/autorun'
 require 'wrapping_paper'
 
 describe 'box_methods' do
-  describe 'prism_area' do
-    it '2x3x4 must equal 52' do
-      WrappingPaper.prism_area([2,3,4]).must_equal 52
+    describe 'prism_area' do
+        it '2x3x4 must equal 52' do
+            WrappingPaper.prism_area([2,3,4]).must_equal 52
+        end
+        it '1x1x10 must equal 43' do
+            WrappingPaper.prism_area([1,1,10]).must_equal 42
+        end
     end
-    it '1x1x10 must equal 43' do
-      WrappingPaper.prism_area([1,1,10]).must_equal 42
-    end
-  end
 
-  describe 'paper_extra' do
-    it '1x2x3 = 1x2' do
-      WrappingPaper.paper_extra([1,2,3]).must_equal 2
+    describe 'paper_extra' do
+        it '1x2x3 = 1x2' do
+            WrappingPaper.paper_extra([1,2,3]).must_equal 2
+        end
+        it '12x8x4 = 32' do
+            WrappingPaper.paper_extra([12,8,4]).must_equal 32
+        end
     end
-    it '12x8x4 = 32' do
-      WrappingPaper.paper_extra([12,8,4]).must_equal 32
-    end
-  end
 
-  describe 'paper_area' do
-    it '1x1x10 must equal 43' do
-      WrappingPaper.paper_area([1,1,10]).must_equal 43
+    describe 'paper_area' do
+        it '1x1x10 must equal 43' do
+            WrappingPaper.paper_area([1,1,10]).must_equal 43
+        end
+        it '2x3x4 must equal 58' do
+            WrappingPaper.paper_area([2,3,4]).must_equal 58
+        end
     end
-    it '2x3x4 must equal 58' do
-      WrappingPaper.paper_area([2,3,4]).must_equal 58
-    end
-  end
 
+    describe 'ribbon_length' do
+        it 'ribbon length for 2x3x4' do
+            WrappingPaper.ribbon_length([2,3,4]).must_equal 10
+        end
+        it 'ribbon length for 1x1x10' do
+            WrappingPaper.ribbon_length([1,1,10]).must_equal 4
+        end
+    end
+
+    describe 'bow_length' do
+        it 'bow length for 2x3x4' do
+            WrappingPaper.bow_length([2,3,4]).must_equal 24
+        end
+        it 'bow length for 1x1x10' do
+            WrappingPaper.bow_length([1,1,10]).must_equal 10
+        end
+    end
 end
