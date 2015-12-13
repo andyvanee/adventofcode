0bfe67258b44cf4575cd0f1c3700fde373af29b4
Andy VanEe <1andyvanee@gmail.com>
2015-12-12 22:27:45 -0700

    Refactor DeliveryRoute to return Route

--- lib/delivery_route.rb
+++ lib/delivery_route.rb
@@ -1,20 +1,28 @@
 require 'set'
 
 module DeliveryRoute
-  def self.process(input)
-    current = [0,0]
-    visited = [current].to_set
-
-    input.split('').each do |token|
-      x,y = current
-      x += 1 if token == '^'
-      x -= 1 if token == 'v'
-      y += 1 if token == '>'
-      y -= 1 if token == '<'
-      current = [x, y]
-      visited.add(current)
+  class Route < ::Set
+    def initialize
+      super([[0,0]])
     end
 
-    visited.length
+    def go(dir)
+      @current ||= [0,0]
+      x,y = @current
+      x += 1 if dir == '^'
+      x -= 1 if dir == 'v'
+      y += 1 if dir == '>'
+      y -= 1 if dir == '<'
+      @current = [x, y]
+      add(@current)
+    end
+  end
+
+  def self.process(input)
+    route = Route.new
+    input.split('').each do |token|
+      route.go(token)
+    end
+    route
   end
 end
--- spec/delivery_route_spec.rb
+++ spec/delivery_route_spec.rb
@@ -3,12 +3,12 @@ require 'delivery_route'
 
 describe DeliveryRoute do
   it '>' do
-    DeliveryRoute.process('>').must_equal 2
+    DeliveryRoute.process('>').length.must_equal 2
   end
   it '^>v<' do
-    DeliveryRoute.process('^>v<').must_equal 4
+    DeliveryRoute.process('^>v<').length.must_equal 4
   end
   it '^v^v^v^v^v' do
-    DeliveryRoute.process('^v^v^v^v^v').must_equal 2
+    DeliveryRoute.process('^v^v^v^v^v').length.must_equal 2
   end
 end
