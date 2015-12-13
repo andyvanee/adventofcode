6ee59d7897f7086cc87b69987db5c81f3dd93101
Andy VanEe <1andyvanee@gmail.com>
2015-12-12 23:07:37 -0700

    Day 4: Step 2

--- lib/advent_coin.rb
+++ lib/advent_coin.rb
@@ -1,10 +1,12 @@
 require 'digest'
 
 module AdventCoin
-  def self.for(input)
+  def self.for(input, len=5)
+    end_range = len - 1
+    init = (0..end_range).map { |i| '0' }.join
     i = 0
     while i = i+1
-      return i if Digest::MD5.hexdigest(input + i.to_s)[0..4] == '00000'
+      return i if Digest::MD5.hexdigest(input + i.to_s)[0..end_range] == init
     end
   end
 end
