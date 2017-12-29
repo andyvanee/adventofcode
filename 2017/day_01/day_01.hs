84e94ed59aa2840115b22e94d5e4edd40df672a8
andyvanee <1andyvanee@gmail.com>
2017-12-29 11:17:27 -0600

    Minor cleanup and comments

--- 2017/day_01/day_01.hs
+++ 2017/day_01/day_01.hs
@@ -1,15 +1,23 @@
 import Data.Char
 
-pairsL x = zip x $ offsetL x
+-- Given a list, return a list of tuples offset by one
+-- [0,1,2] => [(0,2), (1,0), (2,1)]
+numberPairs x = zip x $ offsetList x
 
-offsetL x = (tail x) ++ (head x) : []
+-- Return a list shifted left by one character
+offsetList x = (tail x) ++ (head x) : []
 
-numList l = [digitToInt x | x <- l]
+-- Convert a string into a list of digits
+stringToDigits l = [digitToInt x | x <- l]
 
-sumPair x
+-- Implement the addition rule for the Inverse Captcha
+tupleCaptcha x
     | fst x == snd x = fst x
     | otherwise = 0
 
+-- Sum all the numbers in the input according to the captcha rules
+tupleCaptchaSum x = sum [tupleCaptcha x | x <- numberPairs (stringToDigits x)]
+
 main = do
     input <- getLine
-    print (sum [sumPair x | x <- pairsL (numList input)])
+    print $ tupleCaptchaSum input
