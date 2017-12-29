82b24768970fe9cbf6e285a0c5de5a44e6d78c5f
Andrew VanEe <andy@andrews-mbp.gateway.pace.com>
2017-12-29 11:00:24 -0600

    Messy first version :)

---
+++ 2017/day_01/day_01.hs
@@ -0,0 +1,15 @@
+import Data.Char
+
+pairsL x = zip x (offsetL x)
+
+offsetL x = ((tail x) ++ (head x) : [])
+
+numList l = [digitToInt x | x <- l]
+
+sumPair x
+    | fst x == snd x = fst x
+    | otherwise = 0
+
+main = do
+    input <- getLine
+    print (sum [sumPair x | x <- pairsL (numList input)])
