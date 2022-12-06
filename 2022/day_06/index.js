4fe9a1fdced290bfb2fa5da2b0de1812363093e4
Andy Vanee <andy.vanee@shopify.com>
2022-12-06 10:22:22 -0700

    Typechecking for day 6

--- 2022/day_06/index.js
+++ 2022/day_06/index.js
@@ -1,11 +1,21 @@
 import {HandheldDevice} from '../lib/model/HandheldDevice.js'
 
+/**
+ * Solution for part 1
+ * @param {string} puzzleInput
+ * @returns {number}
+ */
 export const part1 = puzzleInput => {
     const device = new HandheldDevice()
     device.comm.bufferFromString(puzzleInput)
     return device.comm.startOfPacketMarker
 }
 
+/**
+ * Solution for part 2
+ * @param {string} puzzleInput
+ * @returns {number}
+ */
 export const part2 = puzzleInput => {
     const device = new HandheldDevice()
     device.comm.bufferFromString(puzzleInput)
