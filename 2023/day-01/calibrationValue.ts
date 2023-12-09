deadc67efd9510085b81e8ca7c4dce61ca1fb1d1
Andy VanEe <1andyvanee@gmail.com>
2023-12-09 16:41:51 -0700

    Day 1 solutions

--- 2023/day-01/calibrationValue.ts
+++ 2023/day-01/calibrationValue.ts
@@ -1,4 +1,46 @@
 export const calibrationValue = (input: string) => {
     const lines = input.split('\n').filter((line) => line.length)
-    return 142
+    const values = lines.map((line: string) => {
+        // Remove any non-digits in string
+        const digits = line.replace(/[^\d]+/gi, '')
+        return parseInt(`${digits[0]}${digits[digits.length - 1]}`)
+    })
+    return values.reduce((prev, curr) => prev + curr, 0)
+}
+
+const integers = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+}
+
+const integerEntries = Object.entries(integers)
+
+const calibrateWithTextLine = (input: string) => {
+    const values = []
+    let str = input.slice(0)
+    while (str.length) {
+        // text-match
+        const intMatch = integerEntries.find(([k, v]) => str.startsWith(k))
+        if (intMatch) values.push(intMatch[1])
+
+        // numeric-match
+        if (/^\d/.exec(str)) values.push(parseInt(str[0]))
+
+        // reduce string length
+        str = str.slice(1)
+    }
+    return parseInt(`${values[0]}${values[values.length - 1]}`)
+}
+
+export const calibrateWithText = (input: string) => {
+    const lines = input.split('\n').filter((line) => line.length)
+    const values = lines.map(calibrateWithTextLine)
+    return values.reduce((prev, curr) => prev + curr, 0)
 }
--- 2023/day-01/day-01.test.ts
+++ 2023/day-01/day-01.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'bun:test'
-import { calibrationValue } from './calibrationValue'
+import { calibrateWithText, calibrationValue } from './calibrationValue'
 
 const day01Sample = `
 1abc2
@@ -8,6 +8,30 @@ a1b2c3d4e5f
 treb7uchet
 `
 
-test('2 + 2', () => {
+test('day01Sample', () => {
     expect(calibrationValue(day01Sample)).toBe(142)
+})
+
+test('day01Input', async () => {
+    const input = await Bun.file('./day-01/input').text()
+    expect(calibrationValue(input)).toBe(54630)
+})
+
+const day01SampleB = `
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+`
+
+test('day01SampleB', () => {
+    expect(calibrateWithText(day01SampleB)).toBe(281)
+})
+
+test('day01InputB', async () => {
+    const input = await Bun.file('./day-01/input').text()
+    expect(calibrateWithText(input)).toBe(54770)
 })
