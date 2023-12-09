3a81679cbb2c7132abc14144d48b44181da88e3c
Andy VanEe <1andyvanee@gmail.com>
2023-12-09 16:45:00 -0700

    Cleanup

--- 2023/day-01/day-01.test.ts
+++ 2023/day-01/day-01.test.ts
@@ -1,37 +1,22 @@
 import { expect, test } from 'bun:test'
 import { calibrateWithText, calibrationValue } from './calibrationValue'
 
-const day01Sample = `
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
-`
+const input = await Bun.file('./day-01/input').text()
+const sampleA = await Bun.file('./day-01/sampleA').text()
+const sampleB = await Bun.file('./day-01/sampleB').text()
 
 test('day01Sample', () => {
-    expect(calibrationValue(day01Sample)).toBe(142)
+    expect(calibrationValue(sampleA)).toBe(142)
 })
 
 test('day01Input', async () => {
-    const input = await Bun.file('./day-01/input').text()
     expect(calibrationValue(input)).toBe(54630)
 })
 
-const day01SampleB = `
-two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen
-`
-
 test('day01SampleB', () => {
-    expect(calibrateWithText(day01SampleB)).toBe(281)
+    expect(calibrateWithText(sampleB)).toBe(281)
 })
 
 test('day01InputB', async () => {
-    const input = await Bun.file('./day-01/input').text()
     expect(calibrateWithText(input)).toBe(54770)
 })
